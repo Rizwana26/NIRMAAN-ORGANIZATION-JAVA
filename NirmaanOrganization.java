c8c899a2075a848499f8f34c1013b4e1c7122749
Rizwana26 <rizwana7421@gmail.com>
2024-12-06 11:34:06 +0530

    day11-prg2

--- NirmaanOrganization.java
+++ NirmaanOrganization.java
@@ -1,45 +1,19 @@
-package RIZ;
-
-import java.util.Scanner;
+package day11;
 
 public class NirmaanOrganization {
-	  String name="";
-	  int id=0;
-	  int salary=0;
-	  String subject="";
-	  
-	  void checkin() {
-		  System.out.println("Employee here");
-	  }
-	public static void main(String arg[]) {
-		
-		Scanner sc=new Scanner(System.in);
-		NirmaanOrganization emp1=new NirmaanOrganization();
-		System.out.println("enter your name:");
+	public static void main(String[] args) {
+		//Employee emp1= new Employee();
+//		emp1.setEmpName("RAM");
+//		System.out.println(emp1.getEmpName());
+//		
+//		emp1.setEmpId(200);
+//		System.out.println(emp1.getEmpId());
+//		
+//		emp1.setDepartment("Computer Science");
+//		System.out.println(emp1.getDepartment());
+	Employee emp1= new Employee("Ram",203,"Computer Science");
+	System.out.println(emp1);
 
-		emp1.name = sc.nextLine();
-		System.out.println("enter your id:");
-
-		emp1.id=sc.nextInt();
-		System.out.println("enter your salary");
-
-		emp1.salary=sc.nextInt();
-		sc.nextLine();
-		
-		System.out.println("enter your subject:");
-        emp1.subject=sc.nextLine();
-		
-		
-		
-		System.out.println(emp1.name);
-		System.out.println(emp1.id);
-		System.out.println(emp1.salary);
-		System.out.println(emp1.subject);
-		emp1.checkin();
-
-		
-		
-		
 	}
 
 }
