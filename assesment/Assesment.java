0b4caa9aa394f3a3456a7c381c15ec0cbde691e7
Rizwana26 <rizwana7421@gmail.com>
2024-12-30 11:30:23 +0530

    Assesment

---
+++ assesment/Assesment.java
@@ -0,0 +1,82 @@
+package assesment;
+import java .util.Scanner;
+
+public class Assesment {
+	 public static void main(String[] args) {
+		             // 1.EVEN AND COUNT
+	//	  int total=0;
+//		for(int i=0;i<=100;i++) {
+//			if(i%2==0) {
+//				 
+//				
+//				System.out.println(i);
+//				total++;
+//			}
+//			
+//		}
+//		
+//		System.out.println("Total even num:"+total);
+		         //2..ODD  AND COUNT
+//		 for(int i=0;i<=100;i++) {
+//				if(i%2!=0) {
+//					 
+//					
+//					System.out.println(i);
+//					total++;
+//				}
+//				
+//			}
+//		 System.out.println("Total Odd num:"+total);
+		 
+//		 //3.REVERSE A STRING
+//		 Scanner sc= new Scanner(System.in);
+//		 System.out.println("Enter your Name:");
+//				 String name=sc.nextLine();
+//				 int n=name.length();
+//				 for(int i=n-1;i>=0;i--) {
+//					 System.out.print(name.charAt(i));
+//				 }
+//			//	 4.SUM OF EVEN NUM:
+//		 int total=0;
+//			for(int i=0;i<=100;i++) {
+//				if(i%2==0) {
+//					 
+//					
+//					
+//					total+=i;
+//				}
+//				
+//			}
+//			
+//			System.out.println("Total even num:"+total);
+		 
+	//	 5.SUM OF EVEN:
+		 
+	//	 int total=0;
+//			for(int i=0;i<=100;i++) {
+//				if(i%2==0) {
+//					 
+//					
+//					
+//					total+=i;
+//				}
+//				
+//			}
+//			
+//			System.out.println("Total even num:"+total);	
+		 //6. FACTORAL:
+		 int i;
+		 int fact=1;
+		 int a=5;
+		 for(i=1;i<=a;i++) {
+			 fact=fact*i;
+		 }
+		 System.out.println("Factorial of 5: "+fact);
+				 
+				 
+		 
+		 
+	}
+
+}
+
